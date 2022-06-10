336bc8f7b8c1fcff60edb81f95fca49a57e31436
fs2021 <1153539504@qq.com>
2022-06-10 17:50:44 +0800

    1

---
+++ lib/hello.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class Hello {
+  static const MethodChannel _channel = const MethodChannel('hello');
+
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  static String sayHelloFlutterPlugin() {
+    return 'hello flutter_plugin';
+  }
+}
